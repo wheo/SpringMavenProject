cbe0e5a2416e291c6de6362e0a35590ca7607d39
wheo <wheo@tnmtech.co.kr>
2020-01-01 04:52:42 +0900

    mybatis 연동이 안됨

--- src/main/java/com/ihatego3/springMVC/main.java
+++ src/main/java/com/ihatego3/springMVC/main.java
@@ -1,4 +1,4 @@
-package com.ihatego3.springMVCtest;
+package com.ihatego3.springMVC;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/common/dao/AbstractDAO.java
@@ -0,0 +1,9 @@
+package common.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AbstractDAO {
+    protected  Log log = LogFactory.getLog(AbstractDAO.class);
+}
