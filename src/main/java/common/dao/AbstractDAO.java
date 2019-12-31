0f5e3812dc81b4e81f3108b3951bc3ece96c15b3
wheo <wheo@tnmtech.co.kr>
2020-01-01 05:37:24 +0900

    mybatis 해결함 ㅠㅠ web.xml에서 context-param 못불러와서 그랬음. 오타조심!! 로그만 봐도 해결할 수 있음!! classpath 의 위치는 main/java/resources임
    maven project의 구조를 제대로 파악하자

--- src/main/java/common/dao/AbstractDAO.java
+++ src/main/java/common/dao/AbstractDAO.java
@@ -2,8 +2,16 @@ package common.dao;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AbstractDAO {
     protected  Log log = LogFactory.getLog(AbstractDAO.class);
+
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+    // bean 등록이 되면 static으로 쓸 수 있는것 같다. 정의가 어디에 되어있는지!! bean이 뭔지 점점 알아가는 것 같다
+    // 이거 안된 이유는 1차로 web.xml에서 dao-context.xml 를 못불러왔다 오타로 인해 param-name랑 param-value를 써야하는데 param-value만 2개!!!
+
+
 }
